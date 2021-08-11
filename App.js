e0ca1c95375d4fb0b57ade88d7d21d91542c0f8e
beshan <yabeshan@ya.ru>
2021-08-11 20:39:35 +0200

    Android splashscreen

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -10,11 +12,21 @@ import {
   View,
 } from 'react-native';
 
-const App = () => (
-  <View style={{margin:50}}>
-    <Text>Test2</Text>
-    <Image source={require('./house.jpeg')} style={{backgroundColor:'#ff0000',width:100,height:100}}/>
-  </View>
-)
+const App = () => {
+
+  useEffect(()=>{
+    setTimeout( ()=> {
+      SplashScreen.hide();
+    }, 10000);
+  }, []);
+
+
+  return (
+    <View style={{margin:50}}>
+      <Text>Test2</Text>
+      <Image source={require('./house.jpeg')} style={{backgroundColor:'#ff0000',width:100,height:100}}/>
+    </View>
+  );
+};
 
 export default App;
